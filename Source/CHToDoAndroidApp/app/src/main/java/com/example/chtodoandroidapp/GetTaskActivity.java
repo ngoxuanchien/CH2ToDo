a68a5253d81a9c51a1d0007875a769da56c22f04
lyhai1502 <lyhai1502@gmail.com>
2023-06-26 18:33:24 +0700

    create android project and a few of UI

---
+++ Source/CHToDoAndroidApp/app/src/main/java/com/example/chtodoandroidapp/GetTaskActivity.java
@@ -0,0 +1,13 @@
+package com.example.chtodoandroidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_task);
+    }
+}
